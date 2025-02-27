026d676cc769190970d484c42656fcd90bb5586c
Kailash Choudhary <kailash@cimarketing.com.au>
2025-02-27 12:48:46 +0530

    design improvements

--- news/admin.py
+++ news/admin.py
@@ -3,6 +3,10 @@ from wagtail.models import Page
 from .models import NewsIndexPage
 
 NewsIndexPage.content_panels = Page.content_panels + [
+    InlinePanel('newspage_hero', label='Hero Images', panels=[
+        FieldPanel('image'),
+        FieldPanel('title'),
+    ],max_num=1),
     FieldPanel('body'),
 ]
 NewsIndexPage.promote_panels = Page.promote_panels + [
