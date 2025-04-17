f2e13222ff0a3845c74c6aa4a8a1a5e2b67e1382
kailashcimarketing <kailash@cimarketing.com.au>
2025-04-17 10:31:35 +0530

    multi categories select option updated

--- team/models.py
+++ team/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from wagtail.images.models import Image
 from django_extensions.db.fields import AutoSlugField
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from modelcluster.models import ClusterableModel
 
 class Category(models.Model):
     title = models.CharField(null=True,blank=False,max_length=255)
@@ -9,9 +12,25 @@ class Category(models.Model):
     def __str__(self):
         return self.title 
 
+class TeamCategory(Category):
+    page = ParentalKey('Team', related_name='team_category')
+    catgory = models.ForeignKey(
+        'team.category', 
+        null=True, 
+        blank=True, 
+        on_delete=models.SET_NULL, 
+        related_name='+',
+        verbose_name="Category"
+    ) 
+    panels = [
+        # Use a SnippetChooserPanel because blog.BlogAuthor is registered as a snippet
+        FieldPanel("catgory"),
+    ]
+
+
 
 # Create your models here.
-class Team(models.Model):
+class Team(ClusterableModel):
     title = models.CharField(null=True,blank=False,max_length=255)
     profile_picture = models.ForeignKey(
         Image, 
@@ -35,6 +54,20 @@ class Team(models.Model):
     status = models.BooleanField(default=True,blank=True)
     weight = models.IntegerField(default=100,null=False,blank=False)
     additional_information = models.TextField(null=True, blank=True)
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('profile_picture'),
+        FieldPanel('catgory'),
+        InlinePanel('team_category', label='Categories', panels=[
+            FieldPanel('catgory'),
+        ]),
+        FieldPanel('position'),
+        FieldPanel('credentials'),
+        FieldPanel('phone'),
+        FieldPanel('status'),
+        FieldPanel('weight'),
+        FieldPanel('additional_information'),        
+    ] 
     
 
     def __str__(self):
