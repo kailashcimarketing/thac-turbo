210141657eb16d367efa872438d7e736df8d02a2
kailashcimarketing <kailash@cimarketing.com.au>
2025-05-22 14:42:45 +0530

    added owl slider script for coache and instructor list

--- core/static/js/custom-script.js
+++ core/static/js/custom-script.js
@@ -47,7 +47,44 @@ $(window).on('load', function () {
             }
         });
     }
+    if($('.instructor-list').length){    
+        $('.instructor-list .owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:4
+                }
+            }
+        });
+    }
+    if($('.coache-list').length){    
+        $('.coache-list .owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:4
+                }
+            }
+        });
+    }
 });
+
 
 $(function () {
     $('.internal-link').on('click', function (e) {
