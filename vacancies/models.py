93d0e6f6e524e28af529123cd9f88ca5e2f38af0
Kailash Choudhary <kailash@cimarketing.com.au>
2025-06-03 17:12:21 +0530

    Vacancies admin grid status icon

--- vacancies/models.py
+++ vacancies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from wagtail.images.models import Image
 from django_extensions.db.fields import AutoSlugField
 from wagtail.documents.models import Document
-
+from django.utils.html import format_html
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(null=True,blank=False,max_length=255)
@@ -43,4 +43,12 @@ class Vacancies(models.Model):
         verbose_name_plural = "Vacancies"  # Correct plural form
 
     def __str__(self):
-        return self.title 
+        return self.title
+    
+    def status_icon(self):
+        return format_html(
+            '<span style="color:{};">{}</span>',
+            'green' if self.status else 'red',
+            '✓' if self.status else '✗'
+        )
+    status_icon.short_description = 'Status'
